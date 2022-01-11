f938690e7f6f7880237e0731551cbac5ae0edd36
calvinegs <calvin.egs@gmail.com>
2022-01-11 18:53:16 +0800

    add  TodoController

---
+++ Data/ApiDbContext.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Todo5.Models;
+
+#nullable disable
+
+namespace Todo5.Data
+{
+    public partial class ApiDbContext : DbContext
+    {
+        public DbSet<ItemData> ItemData { get; set; }
+        public ApiDbContext()
+        {
+        }
+
+        public ApiDbContext(DbContextOptions<ApiDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlite("Data Source=app.db; Cache=Shared");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ Models/ItemData.cs
@@ -0,0 +1,10 @@
+namespace Todo5.Models
+{
+    public class ItemData
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Details { get; set; }
+        public bool Done { get; set; }
+    }
+}
